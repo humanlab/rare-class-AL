b4dc43874852fe4ec736780f7ff65597e425dfa9
Vase <earthling2304@gmail.com>
2023-06-28 18:30:39 -0400

    extract embeddings added

---
+++ extract_embeddings.py
@@ -0,0 +1,5 @@
+
+
+## use a huggingface transformer to extract embeddings to a file
+## this is a modified version of the script from
+## https://huggingface.co/transformers/quickstart.html#using-the-pipeline
